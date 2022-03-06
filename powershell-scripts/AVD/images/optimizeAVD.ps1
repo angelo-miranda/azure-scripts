693ab0c9325fdcce3813b3ef8893477eae2a84c4
Angelo Miranda <angelocrisanto.miranda@outlook.com>
2022-03-07 04:56:24 +0800

    creation of optimizeAVD.ps1

---
+++ powershell-scripts/AVD/images/optimizeAVD.ps1
@@ -0,0 +1,38 @@
+# Author        : Angelo Miranda
+# Creation Date : 03-07-2022
+# Purpose       : Install OS Optimizations for WVD image (made for Azure Image Builder)
+
+Write-Host 'AIB Customization: Starting OS Optimizations for WVD'
+
+#Set Variables
+$optimizeDirectoryPath     = 'C:\Optimize'
+$osOptimizationDownloadURI = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
+$osOptimizationFileName    = 'Virtual-Desktop-Optimization-Tool-main'
+
+#Create optimize directory
+Write-Host "AIB Customization: Creating directory: "$optimizeDirectoryPath
+New-Item $optimizeDirectoryPath -ItemType Directory -ErrorAction SilentlyContinue
+Set-Location $optimizeDirectoryPath
+Write-Host "AIB Customization: Directory created: "$optimizeDirectoryPath
+
+#Download OS optimization files
+Write-Host "AIB Customization: Downloading OS optimization files to "$optimizeDirectoryPath
+Invoke-WebRequest -Uri $osOptimizationDownloadURI -OutFile "$optimizeDirectoryPath\$osOptimizationFileName.zip"
+Write-Host "AIB Customization: OS optimization files downloaded: "$optimizeDirectoryPath"\"$osOptimizationFileName".zip"
+
+#Extract OS optimization files
+Write-Host "AIB Customization: Extracting files from "$osOptimizationFileName".zip to "$optimizeDirectoryPath
+Unblock-File -LiteralPath "$optimizeDirectoryPath\$osOptimizationFileName.zip"
+Expand-Archive `
+    -LiteralPath "$optimizeDirectoryPath\$osOptimizationFileName.zip" `
+    -DestinationPath $optimizeDirectoryPath `
+    -Force `
+    -Verbose
+Set-Location -LiteralPath "$optimizeDirectoryPath\$osOptimizationFileName"
+Write-Host "AIB Customization: Extracting files from "$osOptimizationFileName".zip to "$optimizeDirectoryPath
+
+# Execute script
+Write-Host 'AIB Customization: Executing OS Optimizations script'
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+.\Win10_VirtualDesktop_Optimize.ps1 -Verbose
+Write-Host 'AIB Customization: Finished OS Optimizations script'
